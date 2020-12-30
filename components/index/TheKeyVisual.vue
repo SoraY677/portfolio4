33f0f4d10d87a6952478e44a940b0ae47a5f84c6
SoraY677 <ys4080@icloud.com>
2020-12-30 15:03:08 +0900

    [update]KeyVisualにリンク他

--- components/index/TheKeyVisual.vue
+++ components/index/TheKeyVisual.vue
@@ -1,6 +1,6 @@
 <template>
-  <v-card min-height="300px" class="key-visual">
-    <v-container class="pa-16">
+  <v-card class="key-visual" min-height="300px" elevation="0">
+    <div class="pa-16 key-visual-inner">
       <!-- 名前 -->
       <div>
         <name></name>
@@ -9,41 +9,79 @@
       <div class="d-flex justify-end">
         <anime class="pirot"></anime>
       </div>
-    </v-container>
-    <div class="circle-block"></div>
+    </div>
+    <div class="key-visual-ornament">
+      <link-list :linkList="linkList" class="link-list mt-5"></link-list>
+      <div class="circle-block">
+        <span class="d-block pb-1">scroll</span>
+        <p class="scroll-arrow"></p>
+      </div>
+    </div>
   </v-card>
 </template>
 
 <script>
 import name from "@/components/index/TheKeyVisualName";
 import anime from "@/components/index/TheKeyVisualAnime";
+import LinkList from "@/components/index/TheKeyVisualLink";
 export default {
   components: {
     name,
     anime,
+    LinkList,
+  },
+  props: {
+    linkList: [],
   },
 };
 </script>
 
 <style scoped>
 .key-visual {
-  background-color: var(--main-color);
   color: var(--bg-color);
+}
+
+.key-visual-inner {
+  width: 100%;
+  background-color: var(--main-color);
+}
+.pirot {
+  z-index: 100;
+  width: fit-content;
+  margin: 0;
+}
+
+.key-visual-ornament {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
 }
 
 .circle-block {
-  position: absolute;
-  width: 200px;
-  height: 200px;
-  border-radius: 50% !important;
+  display: flex;
   left: 50%;
-  transform: translateX(-50%);
+  width: 200px;
+  height: 100px;
+  border-radius: 0 0 100px 100px;
+  justify-self: center;
+  justify-content: flex-start;
+  align-items: center;
+  flex-direction: column;
+  flex-wrap: wrap;
   background-color: var(--main-color);
   color: var(--bg-color);
 }
-
-.pirot {
-  width: fit-content;
+.scroll-arrow {
+  height: 3em;
+  width: 1em;
+  border-left: 2px solid var(--bg-color);
+  border-bottom: 2px solid var(--bg-color);
   margin: 0;
+  transform: skewY(-45deg) translateX(50%);
+}
+
+.link-list {
+  height: 2.5em;
+  padding-left: 2em;
+  padding-right: 2em;
 }
 </style>
---
+++ components/index/TheKeyVisualLink.vue
@@ -0,0 +1,33 @@
+<template>
+  <ul class="d-flex justify-sm-space-between">
+    <li v-for="item in linkList" :key="item.id">
+      <a :href="item.url" class="d-inline-block">
+        <img :src="item.path" />
+      </a>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: {
+    linkList: [],
+  },
+};
+</script>
+
+<style scoped>
+li {
+  display: flex;
+  justify-content: center;
+}
+
+a {
+  display: inline-block;
+  height: 100%;
+}
+
+img {
+  height: 100%;
+}
+</style>
