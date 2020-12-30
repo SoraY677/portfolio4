941fdbbf1f48573d4d600774f8d0fa575c386b7b
SoraY677 <ys4080@icloud.com>
2020-12-30 15:07:50 +0900

    [add]紹介文の追加

---
+++ components/index/TheIntroduction.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-container class="d-flex justify-space-around">
+    <img src="alien.svg" class="icon" />
+    <div class="text-container">
+      <p class="pa-3">{{ introText }}</p>
+    </div>
+  </v-container>
+</template>
+
+<script>
+export default {
+  props: {
+    introText: "",
+  },
+};
+</script>
+
+<style scoped>
+.icon {
+  width: 20%;
+}
+
+.text-container {
+  width: 68%;
+  border: 4px solid var(--main-color);
+  border-radius: 1em;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,42 @@
 <template>
   <section>
-    <key-visual></key-visual>
+    <key-visual :linkList="linkList" class="mb-12"></key-visual>
+    <intro :introText="introText" class="px-10"></intro>
   </section>
 </template>
 
 <script>
 import KeyVisual from "@/components/index/TheKeyVisual.vue";
+import Intro from "@/components/index/TheIntroduction.vue";
 export default {
   components: {
     KeyVisual,
+    Intro,
+  },
+  data() {
+    return {
+      linkList: [
+        {
+          name: "twitter",
+          url: "https://twitter.com/TenYDGB",
+          path: "/link/twitter.svg",
+        },
+        {
+          name: "Github",
+          url: "https://github.com/SoraY677",
+          path: "/link/github.svg",
+        },
+        {
+          name: "Qiita",
+          url: "https://qiita.com/SoraYamaguchi",
+          path: "/link/qiita.svg",
+        },
+      ],
+      introText: "とりあえずてすとな",
+    };
   },
 };
 </script>
 
-<style>
+<style scoped>
 </style>
