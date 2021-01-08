6796d91ad11b62ec6a1d4944744b94c56fb1b409
SoraY677 <ys4080@icloud.com>
2021-01-08 23:49:48 +0900

    menu作成

--- components/Menu.vue
+++ components/Menu.vue
@@ -1,7 +1,11 @@
 <template>
   <nav>
-    <v-app-bar-nav-icon @click.stop="drawer = !drawer" class="menu-trigger">
-    </v-app-bar-nav-icon>
+    <v-button @click.stop="drawer = !drawer" class="menu-trigger">
+      <div class="menu-trigger-inner">
+        <img src="/moon.svg" height="30px" width="30px" class="menu-icon" />
+        <p class="menu-supplement mb-0 text-center">menu</p>
+      </div>
+    </v-button>
 
     <v-navigation-drawer
       v-model="drawer"
@@ -12,7 +16,7 @@
       class="menu-container"
     >
       <v-container class="pa-10 panel-full menu-content">
-        <h1 class="text-center text-h2">Menu</h1>
+        <h1 class="text-bg-color text-center text-h2">Menu</h1>
         <div class="list-container">
           <v-list
             class="wh-full d-flex justify-center align-center flex-wrap pa-10"
@@ -67,9 +71,37 @@ export default {
 <style scoped>
 .menu-trigger {
   position: fixed;
+  display: flex;
   z-index: 201;
-  top: 5px;
-  left: 5px;
+  top: 0px;
+  left: 0px;
+  padding: 2px;
+  border-radius: 0 0 10px 0;
+  justify-content: center;
+  align-items: center;
+  background-color: var(--main-color);
+}
+
+.menu-trigger-inner {
+  width: 100%;
+  height: 100%;
+  padding: 2px;
+  transition-duration: 0.3s;
+}
+
+.menu-trigger-inner:hover {
+  background-color: rgba(250, 250, 250, 0.3);
+  box-shadow: 2px 2px 0px 0px var(--bg-color);
+}
+
+.menu-icon {
+  height: 2em !important;
+  width: 2em !important;
+  transition-duration: 0.3s;
+}
+.menu-supplement {
+  color: var(--bg-color);
+  font-size: 0.4em;
 }
 
 .menu-container {
@@ -101,13 +133,14 @@ export default {
   height: 100%;
   color: var(--bg-color);
   text-decoration: none;
-  overflow: hidden;
   text-align: center;
+  overflow: hidden;
   font-size: 1.2em;
 }
 
 .link-item:hover {
   color: var(--main-color);
+  font-weight: bold;
 }
 
 .link-item::before {
