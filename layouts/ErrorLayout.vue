2429bb4e45fcfd8d3f5a9113327a31d69cdb6020
SoraY677 <ys4080@icloud.com>
2021-01-10 03:35:47 +0900

    [update]Errorページを作成

---
+++ layouts/ErrorLayout.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app>
+    <nuxt class="n-container"></nuxt>
+  </v-app>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.n-container {
+  height: 100vh;
+  width: 100vw;
+}
+</style>
