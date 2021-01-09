443c605dbac7e390ceb0b01fa848184078a2cce0
SoraY677 <ys4080@icloud.com>
2021-01-10 03:36:12 +0900

    blogのページを作成中に

--- pages/blog.vue
+++ pages/blog.vue
@@ -1,5 +1,15 @@
 <template>
-  <section></section>
+  <section
+    class="mx-5 d-flex justify-center align-center flex-wrap text-h4 text-sm-h2"
+  >
+    <div class="text-container">
+      <h1 class="mb-4">Under🛸Construction</h1>
+      <div>
+        <p>現在工事中デス...ゴメンナサイ！</p>
+        <p>近日公開デス。コウ、ゴキタイ！</p>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
@@ -12,4 +22,12 @@ export default {
 </script>
 
 <style scoped>
+section {
+  border: 3px solid var(--main-color);
+  border-radius: 30px;
+}
+
+.text-container {
+  font-size: 0.6em;
+}
 </style>
