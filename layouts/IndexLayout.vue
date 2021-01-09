345623c4727df8f140efbe4d2d88e0432dd6309a
SoraY677 <ys4080@icloud.com>
2021-01-10 02:43:36 +0900

    [update]topページにフッター追加

--- layouts/IndexLayout.vue
+++ layouts/IndexLayout.vue
@@ -2,16 +2,19 @@
   <v-app>
     <l-menu></l-menu>
     <nuxt></nuxt>
+    <my-footer class="mt-16"></my-footer>
   </v-app>
 </template>
 
 <script>
 import MyHeader from "@/components/TheHeader.vue";
 import LMenu from "~/layouts/Menu.vue";
+import MyFooter from "@/components/TheFooter.vue";
 export default {
   components: {
     MyHeader,
     LMenu,
+    MyFooter,
   },
 };
 </script>
