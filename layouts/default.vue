d21f959ca1f1aa3268f4f715e4e7b5bab532abcf
SoraY677 <ys4080@icloud.com>
2021-01-10 02:41:21 +0900

    [add]Footerの追加

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,20 +2,26 @@
   <v-app>
     <l-menu></l-menu>
     <my-header class="mb-16"></my-header>
-    <nuxt></nuxt>
+    <nuxt class="n-content"></nuxt>
+    <my-footer class="mt-16"></my-footer>
   </v-app>
 </template>
 
 <script>
 import MyHeader from "@/components/TheHeader.vue";
 import LMenu from "~/layouts/Menu.vue";
+import MyFooter from "@/components/TheFooter.vue";
 export default {
   components: {
     MyHeader,
     LMenu,
+    MyFooter,
   },
 };
 </script>
 
 <style>
+.n-content {
+  min-height: 60vh;
+}
 </style>
